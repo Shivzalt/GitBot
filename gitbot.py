427d89d92f47dacdb1d5f114b47fd625e5f32d2c
Shivzalt <shivzalt@gmail.com>
2023-01-01 23:12:32 +0530

    Added Sunday, January 01, 2023 to file.txt

--- gitbot.py
+++ gitbot.py
@@ -1,11 +1,17 @@
 import os
 from random import randint
-for i in range(1,365):
-    for j in range(1,10):
-        d=str(i)+" days ago"
+import datetime
+
+start_date = datetime.date(2023, 1, 1)
+end_date = datetime.date(2023, 12, 31)
+
+for i in range(int((end_date - start_date).days)):
+    date = start_date + datetime.timedelta(days=i)
+    for j in range(10):
+        d = date.strftime("%A, %B %d, %Y")
         with open('file.txt', 'a') as f:
             f.write(d)
         os.system('git add .')
         commit_message = f"Added {d} to file.txt"
         os.system(f'git commit --date="{d}" -m "{commit_message}"')
-os.system('git push origin main')
+    os.system('git push origin main')
